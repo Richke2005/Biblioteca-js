906119e28f32118963b3067045625e96c55361ac
Richke2005 <richardke18@gmail.com>
2023-07-30 20:45:08 -0300

    Add cors necessary to allow the conection from front - 30/07

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const rotaRoupas = require('./routes/index')
+const cors = require('cors')
 
 const app = express()
+
 app.use(express.json())
+
+app.use(cors({origin: "*"}))
 
 app.use('/roupas', rotaRoupas)
 
